82b23846055fd8f3aba49e8d6426c15fd76a65fb
Hack-Zac <isaacjasonabraham@gmail.com>
2026-03-12 21:14:18 +0800

    Update TypicalPersons.java

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -26,22 +26,22 @@ public class TypicalPersons {
     public static final Person ALICE = new PersonBuilder().withName("Alice Pauline")
         .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
         .withPhone("94351253")
-        .withTags("friends").withMembershipId(1000).build();
+        .withTags("friends").withMembershipId(MembershipId.MIN_ID).build();
     public static final Person BENSON = new PersonBuilder().withName("Benson Meier")
         .withAddress("311, Clementi Ave 2, #02-25")
         .withEmail("johnd@example.com").withPhone("98765432")
-        .withTags("owesMoney", "friends").withMembershipId(1001).build();
+        .withTags("owesMoney", "friends").withMembershipId(MembershipId.MIN_ID).build();
     public static final Person CARL = new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
-        .withEmail("heinz@example.com").withAddress("wall street").withMembershipId(1002).build();
+        .withEmail("heinz@example.com").withAddress("wall street").withMembershipId(MembershipId.MIN_ID).build();
     public static final Person DANIEL = new PersonBuilder().withName("Daniel Meier").withPhone("87652533")
         .withEmail("cornelia@example.com").withAddress("10th street").withTags("friends")
-        .withMembershipId(1003).build();
+        .withMembershipId(MembershipId.MIN_ID).build();
     public static final Person ELLE = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
-        .withEmail("werner@example.com").withAddress("michegan ave").withMembershipId(1004).build();
+        .withEmail("werner@example.com").withAddress("michegan ave").withMembershipId(MembershipId.MIN_ID).build();
     public static final Person FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
-        .withEmail("lydia@example.com").withAddress("little tokyo").withMembershipId(1005).build();
+        .withEmail("lydia@example.com").withAddress("little tokyo").withMembershipId(MembershipId.MIN_ID).build();
     public static final Person GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
-        .withEmail("anna@example.com").withAddress("4th street").withMembershipId(1006).build();
+        .withEmail("anna@example.com").withAddress("4th street").withMembershipId(MembershipId.MIN_ID).build();
 
 
 
