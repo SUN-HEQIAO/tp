11b3873235c1651e8385ca3763b1b55880a9e75f
SUN_HEQIAO <e1355839@u.nus.edu>
2026-04-14 00:17:07 +0800

    Fix issue 368

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -77,17 +77,27 @@ public class MainApp extends Application {
 
         Optional<ReadOnlyAddressBook> addressBookOptional;
         ReadOnlyAddressBook initialData;
+        boolean isSampleData = false;
         try {
             addressBookOptional = storage.readAddressBook();
             if (!addressBookOptional.isPresent()) {
                 logger.info("Creating a new data file " + storage.getAddressBookFilePath()
                         + " populated with a sample AddressBook.");
+                isSampleData = true;
             }
             initialData = addressBookOptional.orElseGet(SampleDataUtil::getSampleAddressBook);
         } catch (DataLoadingException e) {
             logger.warning("Data file at " + storage.getAddressBookFilePath() + " could not be loaded."
                     + " Will be starting with an empty AddressBook.");
             initialData = new AddressBook();
+        }
+        
+        if (isSampleData) {
+            try {
+                storage.saveAddressBook(initialData);
+            } catch (IOException e) {
+                logger.warning("Failed to save sample address book : " + StringUtil.getDetails(e));
+            }
         }
 
         return new ModelManager(initialData, userPrefs);
